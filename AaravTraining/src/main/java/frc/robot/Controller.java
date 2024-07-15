fccba616c237e2d9d8a4703275eebeca274efaa5
Aarav Agarwal <aarav.agarwal.000@outlook.com>
2024-07-15 19:14:21 -0400

    Imported Code From Day I Missed (Explained)

---
+++ AaravTraining/src/main/java/frc/robot/Controller.java
@@ -0,0 +1,100 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+
+/** Add your docs here. */
+public class Controller {
+
+  private static final String MANIP_NAME = "Logitech Dual Action";
+  private static final String DRIVER_NAME = "Controller (Xbox One For Windows)";
+  private static final int UNKNOWN_PORT = -1;
+
+  /* Instance variables */
+  private XboxController manipulatorController;
+  private XboxController driverController;
+  private int manipulatorPort;
+  private int driverPort;
+
+  /* Constructor */
+  public Controller() {
+
+    manipulatorPort = getManipulatorPort();
+    driverPort = getDriverPort();
+
+    if (manipulatorPort != UNKNOWN_PORT) {
+      manipulatorController = new XboxController(manipulatorPort);
+    }
+
+    if (driverPort != UNKNOWN_PORT) {
+      driverController = new XboxController(driverPort);
+    }
+
+    /*
+     * Debug to get actual controller names for constants above.
+     * This can be commented once we have the names.
+     */
+    testPrintControllerNames();
+  }
+
+  public int getManipulatorPort() {
+    int portNum;
+    GenericHID genericController;
+
+    for (portNum = 0; portNum < 6; portNum++) {
+      genericController = new GenericHID(portNum);
+      if (genericController.getName().equals(MANIP_NAME) == true) {
+        return portNum;
+      }
+    }
+
+    System.out.println("ERROR: Could not determine Manipulator Controller USB port!!!");
+    return UNKNOWN_PORT;
+  }
+
+  public int getDriverPort() {
+    int portNum;
+    GenericHID genericController;
+
+    for (portNum = 0; portNum < 6; portNum++) {
+      genericController = new GenericHID(portNum);
+      if (genericController.getName().equals(DRIVER_NAME) == true) {
+        return portNum;
+      }
+    }
+
+    System.out.println("ERROR: Could not determine Driver Controller USB port!!!");
+    return UNKNOWN_PORT;
+  }
+
+  public void updateControllers() {
+    int currentManipulatorPort;
+    int currentDriverPort;
+
+    currentManipulatorPort = getManipulatorPort();
+    currentDriverPort = getDriverPort();
+
+    if (manipulatorPort != currentManipulatorPort) {
+      manipulatorController = new XboxController(currentManipulatorPort);
+      manipulatorPort = currentManipulatorPort;
+      System.out.println("Manipulator Controller now on port:" + manipulatorPort);
+    }
+
+    if (driverPort != currentDriverPort) {
+      driverController = new XboxController(currentDriverPort);
+      driverPort = currentDriverPort;
+      System.out.println("Driver Controller now on port:" + driverPort);
+    }
+  }
+
+  public void testPrintControllerNames() {
+    int portNum;
+    GenericHID genericController;
+
+    for (portNum = 0; portNum < 6; portNum++) {
+      genericController = new GenericHID(portNum);
+      System.out.println("Port:" + portNum + "Controller Name:" + genericController.getName());
+    }
+  }
+
+}
--- AaravTraining/src/main/java/frc/robot/Robot.java
+++ AaravTraining/src/main/java/frc/robot/Robot.java
@@ -18,123 +18,123 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-    private static final String kDefaultAuto = "Default";
-    private static final String kCustomAuto = "My Auto";
-    private String m_autoSelected;
-    private final SendableChooser<String> m_chooser;
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser;
 
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any
-     * initialization code.
-     */
-    public Robot() {
-        m_chooser = new SendableChooser<>();
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any
+   * initialization code.
+   */
+  public Robot() {
+    m_chooser = new SendableChooser<>();
+  }
+
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    m_chooser.addOption("My Auto2", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
+   *
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
     }
+  }
 
-    @Override
-    public void robotInit() {
-        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-        m_chooser.addOption("My Auto", kCustomAuto);
-        m_chooser.addOption("My Auto2", kCustomAuto);
-        SmartDashboard.putData("Auto choices", m_chooser);
-    }
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {
+  }
 
-    /**
-     * This function is called every 20 ms, no matter the mode. Use this for items
-     * like diagnostics
-     * that you want ran during disabled, autonomous, teleoperated and test.
-     *
-     * <p>
-     * This runs after the mode specific periodic functions, but before LiveWindow
-     * and
-     * SmartDashboard integrated updating.
-     */
-    @Override
-    public void robotPeriodic() {
-    }
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
 
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different
-     * autonomous modes using the dashboard. The sendable chooser code works with
-     * the Java
-     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-     * chooser code and
-     * uncomment the getString line to get the auto name from the text box below the
-     * Gyro
-     *
-     * <p>
-     * You can add additional auto modes by adding additional comparisons to the
-     * switch structure
-     * below with additional strings. If using the SendableChooser make sure to add
-     * them to the
-     * chooser code above as well.
-     */
-    @Override
-    public void autonomousInit() {
-        m_autoSelected = m_chooser.getSelected();
-        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-        System.out.println("Auto selected: " + m_autoSelected);
-    }
+  }
 
-    /** This function is called periodically during autonomous. */
-    @Override
-    public void autonomousPeriodic() {
-        switch (m_autoSelected) {
-            case kCustomAuto:
-                // Put custom auto code here
-                break;
-            case kDefaultAuto:
-            default:
-                // Put default auto code here
-                break;
-        }
-    }
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {
+  }
 
-    /** This function is called once when teleop is enabled. */
-    @Override
-    public void teleopInit() {
-    }
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {
+  }
 
-    /** This function is called periodically during operator control. */
-    @Override
-    public void teleopPeriodic() {
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {
+    SmartDashboard.putNumber("Hi!!!", 0.99);
+  }
 
-    }
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {
+    System.out.println("Hello World");
+    double hiKeyValue = SmartDashboard.getNumber("Hi!!!", 0.00);
+    SmartDashboard.putNumber("fgaghsghs", hiKeyValue);
+    System.out.println(m_chooser.getSelected());
+  }
 
-    /** This function is called once when the robot is disabled. */
-    @Override
-    public void disabledInit() {
-    }
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {
+  }
 
-    /** This function is called periodically when disabled. */
-    @Override
-    public void disabledPeriodic() {
-    }
-
-    /** This function is called once when test mode is enabled. */
-    @Override
-    public void testInit() {
-        SmartDashboard.putNumber("Hi!!!", 0.99);
-    }
-
-    /** This function is called periodically during test mode. */
-    @Override
-    public void testPeriodic() {
-        System.out.println("Hello World");
-        double hiKeyValue = SmartDashboard.getNumber("Hi!!!", 0.00);
-        SmartDashboard.putNumber("fgaghsghs", hiKeyValue);
-        System.out.println(m_chooser.getSelected());
-    }
-
-    /** This function is called once when the robot is first started up. */
-    @Override
-    public void simulationInit() {
-    }
-
-    /** This function is called periodically whilst in simulation. */
-    @Override
-    public void simulationPeriodic() {
-    }
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {
+  }
 }
